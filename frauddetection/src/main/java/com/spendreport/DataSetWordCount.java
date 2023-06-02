cbf3ce51eea9c63f934acb0ed8e7013b10228c67
pisiliang <2282561186@qq.com>
2023-06-02 16:34:24 +0800

    Initial commit

--- frauddetection/src/main/java/com/spendreport/DataSetWordCount.java
+++ frauddetection/src/main/java/com/spendreport/DataSetWordCount.java
@@ -1,15 +1,10 @@
 package com.spendreport;
 
-import org.apache.flink.api.common.functions.FlatMapFunction;
-import org.apache.flink.api.java.DataSet;
-import org.apache.flink.api.java.ExecutionEnvironment;
-import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
-import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.util.Collector;
 
-public class DataSetWordcount {
+public class DataSetWordCount {
+
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         DataStreamSource<String> source = env.socketTextStream("localhost", 9999);
