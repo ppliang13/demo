1518fb1fa7bc9cd2d8c4e53551718ad69aae9053
pisiliang <2282561186@qq.com>
2023-06-05 14:42:09 +0800

    flink-demo

---
+++ flink-demo/src/main/java/com/ppl/flinkdemo/FlinkApplication.java
@@ -0,0 +1,13 @@
+package com.ppl.flinkdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlinkApplication.class, args);
+    }
+
+}
