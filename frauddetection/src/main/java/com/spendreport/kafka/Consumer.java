5c10d7f7d753732bcfc78b6842f9a7435eb9b68f
pisiliang <2282561186@qq.com>
2023-06-05 14:38:54 +0800

    delete

--- frauddetection/src/main/java/com/spendreport/kafka/Consumer.java
+++
@@ -1,8 +0,0 @@
-package com.spendreport.kafka;
-
-/**
- * Created by pisiliang on 2023/6/2 15:05
- */
-public class Consumer {
-
-}
--- frauddetection/src/main/java/com/spendreport/save/SqlSave.java
+++
@@ -1,7 +0,0 @@
-package com.spendreport.save;
-
-/**
- * Created by pisiliang on 2023/6/1 14:24
- */
-public class SqlSave {
-}
